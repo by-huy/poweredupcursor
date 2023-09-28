2ba54094a3269dc5f50c4f7412b9637526936169
Huy Nguyen <113236181+cheesezzzz@users.noreply.github.com>
2023-09-28 11:15:23 +1000

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,69 @@
-import { useRef } from "react"
-import Cursor from "./components/Cursor"
+import { useRef } from "react";
+import Cursor from "./components/Cursor";
 
 export default function App() {
-
-  const linksRef = useRef([])
-  const videoRef = useRef(null)
+  const linksRef = useRef([]);
+  const videoRef = useRef(null);
 
   const links = [
     {
       label: "Youtube",
-      href: 'https://www.youtube.com/channel/UCBOAB9RV647G93GxLhEXleA'
+      href: "https://www.youtube.com/channel/UCBOAB9RV647G93GxLhEXleA",
     },
     {
       label: "Linkedin",
-      href: 'https://www.linkedin.com/in/huyngxyz/'
+      href: "https://www.linkedin.com/in/huyngxyz/",
     },
     {
-      label: 'Bento',
-      href: 'https://www.bento.me/huyng'
-    }
-
-  ]
-
+      label: "Bento",
+      href: "https://www.bento.me/huyng",
+    },
+  ];
 
   return (
     <main className=" font-borna flex-col flex h-screen items-center justify-center">
-      <Cursor linksRef={linksRef} videoRef={videoRef}/>
-      <h1 className="uppercase tracking-tighter text-5xl">Custom Cursor Effect</h1>
-      <video ref={videoRef}>
-        <source src="https://www.youtube.com/watch?v=AJdTBPuZkHU" type="" />
-      </video>
+      <Cursor linksRef={linksRef} videoRef={videoRef} />
+      <h1 className="uppercase tracking-tighter text-5xl">
+        Custom Cursor Effect
+      </h1>
+      <a
+        href="https://www.youtube.com/channel/UCBOAB9RV647G93GxLhEXleA"
+        className="my-20 h-72 gap-x-4 rounded-md item flex max-w-5xl mx-auto cursor-pointer"
+        ref={videoRef}
+      >
+        <div className=" overflow-hidden">
+          <img
+            className="object-cover object-center"
+            src="https://images.unsplash.com/photo-1584448098255-234156529929?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2582&q=80"
+            alt=""
+          />
+        </div>
+        <div>
+          <span className="text-base text-gray-600">Insights</span>
+          <h2 className="text-4xl pb-4">
+            Nostalgia: Exploring the Resurgence of Retro Culture
+          </h2>
+          <p>
+            Dive into the enchanting world of retro nostalgia as we uncover the
+            cultural revival of vintage aesthetics and their impact on modern
+            trends. From fashion to music and everything in between, discover
+            how the past is shaping our present in unexpected ways.
+          </p>
+        </div>
+      </a>
       <div className="space-x-6">
         {links.map((link, index) => {
           return (
-            <a ref={(ref) => (linksRef.current[index] = ref)} key={index} href={link.href}>{link.label}</a>
-          )
+            <a
+              ref={(ref) => (linksRef.current[index] = ref)}
+              key={index}
+              href={link.href}
+            >
+              {link.label}
+            </a>
+          );
         })}
       </div>
     </main>
-  )
+  );
 }
--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -5,15 +5,20 @@ import { motion, useMotionValue, useSpring } from "framer-motion";
 export default function Cursor({ linksRef, videoRef }) {
   const [cursorText, setCursorText] = useState("");
   const [cursorVariant, setCursorVariant] = useState("default");
-  const [isHovered, setIsHovered] = useState(false)
+  const [isHovered, setIsHovered] = useState(false);
+  const [isVideoHovered, setIsVideoHovered] = useState(false)
 
   // Cursor Logic & Tracking
-  const cursorSize = isHovered ? 48 : 16;
+  const cursorSize = isHovered ? (isVideoHovered ? 100 : 56) : 16;
+
+
+  //   setting the initial positions of the cursor
   const cursor = {
     y: useMotionValue(0),
     x: useMotionValue(0),
   };
 
+  //   Transition configuration
   const spring = {
     type: "spring",
     stiffness: 500,
@@ -21,11 +26,13 @@ export default function Cursor({ linksRef, videoRef }) {
     mass: 0.5,
   };
 
+  //   Smoothing the mouse cursor when moved
   const smoothMouse = {
     x: useSpring(cursor.x, spring),
-    y: useSpring(cursor.y, spring)
-  }
+    y: useSpring(cursor.y, spring),
+  };
 
+  //   Tracking the cursor
   const trackCursor = (e) => {
     const { clientY, clientX } = e;
     cursor.y.set(clientY - cursorSize / 2);
@@ -41,38 +48,64 @@ export default function Cursor({ linksRef, videoRef }) {
     links: {
       width: cursorSize,
       height: cursorSize,
-      backgroundColor: '#618264'
+      backgroundColor: "#618264",
+    },
+    video: {
+      width: cursorSize,
+      height: cursorSize, 
+      backgroundColor: "#3085C3",
     },
   };
 
-  
-//   Hovering functions
+  //   Hovering functions
   const linkOver = () => {
-    setCursorText('👋')
-    setCursorVariant("links")
-    setIsHovered(true)
-  }
+    setCursorText("👋");
+    setCursorVariant("links");
+    setIsHovered(true);
+  };
 
   const linkLeave = () => {
-    setCursorText('')
-    setCursorVariant("default")
-    setIsHovered(false)
-  }
+    setCursorText("");
+    setCursorVariant("default");
+    setIsHovered(false);
+  };
+
+  const videoOver = () => {
+    setCursorText("view");
+    setCursorVariant("video");
+    setIsVideoHovered(true)
+    setIsHovered(true);
+
+  };
+
+  const videoLeave = () => {
+    setCursorText("");
+    setCursorVariant("default");
+    setIsVideoHovered(false)
+    setIsHovered(false);
+
+  };
 
   useEffect(() => {
-    linksRef.current.forEach((link) => {
-        link.addEventListener("mouseover", linkOver)
-        link.addEventListener("mouseleave", linkLeave)
-    })
+    console.log(videoRef.current);
+
     window.addEventListener("mousemove", trackCursor);
+    linksRef.current.forEach((link) => {
+      link.addEventListener("mouseover", linkOver);
+      link.addEventListener("mouseleave", linkLeave);
+    });
+    videoRef.current.addEventListener("mouseover", videoOver);
+    videoRef.current.addEventListener("mouseleave", videoLeave);
 
     // Cleaning up the listeners after it has unmounted
     return () => {
       window.removeEventListener("mousemove", trackCursor);
       linksRef.current.forEach((link) => {
-        link.removeEventListener("mouseover", linkOver)
-        link.removeEventListener("mouseleave", linkLeave)
-    })
+        link.removeEventListener("mouseover", linkOver);
+        link.removeEventListener("mouseleave", linkLeave);
+      });
+      videoRef.current.addEventListener("mouseover", videoOver);
+      videoRef.current.addEventListener("mouseleave", videoLeave);
     };
   });
 
@@ -85,7 +118,7 @@ export default function Cursor({ linksRef, videoRef }) {
         style={{ left: smoothMouse.x, top: smoothMouse.y }}
         className="z-[999] w-4 h-4 bg-red-500 rounded-full fixed pointer-events-none flex justify-center items-center"
       >
-        <span>{cursorText}</span>
+        <span className="text-xs">{cursorText}</span>
       </motion.div>
     </div>
   );
